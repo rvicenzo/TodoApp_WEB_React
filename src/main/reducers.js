32817e84db62be93d7e1d97e53e1c6d740b3e06c
Rafael Vicenzo <rafa.vicenzo@gmail.com>
2019-05-03 12:49:11 -0300

    search, add task, aplicado middlewares

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,26 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: 'Ler',
-        list: [
-            {
-                _id: 1,
-                description: 'Pagar fatura',
-                done: true
-            },
-            {
-                _id: 2,
-                description: 'Reunião com a equipe',
-                done: false
-            },
-            {
-                _id: 3,
-                description: 'Consulta médica',
-                done: false
-            }
-        ]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,40 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators} from 'redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-const TodoForm = props => {
+import { changeDescription, search, add } from './todoActions'
 
-    const keyHandler = (e) => {
+export class TodoForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    componentWillMount() {
+        this.props.search()
+    }
+
+    keyHandler(e) {
+        const { search, add, description } = this.props
         if(e.key === 'Enter') {
-            e.shiftKey ? props.handleSearch() : props.handleAdd() 
+            e.shiftKey ? search() : add(description) 
         } else if(e.key === 'Escape') {
             props.handleClear()
         }
     }
 
-    return (
-        <div role="form" className="todoForm">    
-            <Grid cols="12 9 10">
-                <input id="description" 
-                    className="form-control"
-                    placeholder="Adionar tarefa"
-                    onKeyUp={keyHandler}
-                    onChange={props.handleChange}
-                    value={props.description}
-                    >
-                </input>
-            </Grid>
-            <Grid cols="12 3 2">
-                <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
-                <IconButton style="info" icon="search" onClick={props.handleSearch} />
-                <IconButton style="default" icon="close" onClick={props.handleClear} />
-            </Grid>
-        </div>
-    )
+    render() {
+        const { search, add, description } = this.props
+        return (
+            <div role="form" className="todoForm">    
+                <Grid cols="12 9 10">
+                    <input id="description" 
+                        className="form-control"
+                        placeholder="Adionar tarefa"
+                        onKeyUp={this.keyHandler}
+                        onChange={this.props.changeDescription}
+                        value={this.props.description}
+                        >
+                    </input>
+                </Grid>
+                <Grid cols="12 3 2">
+                    <IconButton style="primary" icon="plus" onClick={ () => add(description) } />
+                    <IconButton style="info" icon="search" onClick={() => search()} />
+                    <IconButton style="default" icon="close" onClick={this.props.handleClear} />
+                </Grid>
+            </div>
+        )
+    }
+
 }
 
 // obter os atributos do componente na store e mapear dentro de props
 const mapStateToProps = state => ({ description: state.todo.description })
-
-export default connect(mapStateToProps)(TodoForm)
+const mapDispatchToProps = dispatch => 
+    bindActionCreators({ changeDescription, search, add }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
